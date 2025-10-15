1a935cca4970d0d83c6455768236186f2653a4f3
p017395 <jhvsilva@prodam.sp.gov.br>
2025-10-15 12:35:33 -0300

    terraform github actions

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -15,6 +15,13 @@ terraform {
       version = "~> 3.2"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatecleanarch"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
